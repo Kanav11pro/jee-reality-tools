cf5091ad8aa4e40f15e42b04adc22470a7f0da80
Kanav11pro <cbforin@gmail.com>
2025-10-05 01:20:41 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,15 @@
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import StudyTools from './pages/StudyTools'
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/tools" element={<StudyTools />} />
+    </Routes>
+  )
+}
+
+export default App
 
