5c2b3678da071f36fc8d4482eeddebf9445d6dc7
Kanav11pro <cbforin@gmail.com>
2025-10-05 12:57:57 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
-import StudyTools from './pages/StudyTools'
+import RealityChecks from './pages/RealityChecks'
+import SpeedGames from './pages/SpeedGames'
+import PlanningTools from './pages/PlanningTools'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/tools" element={<StudyTools />} />
+      <Route path="/reality-checks" element={<RealityChecks />} />
+      <Route path="/speed-games" element={<SpeedGames />} />
+      <Route path="/planning-tools" element={<PlanningTools />} />
     </Routes>
   )
 }
 
 export default App
-
